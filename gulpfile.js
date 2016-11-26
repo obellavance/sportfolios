842efd9eed2f0f14765ed8dd8f62de1a6b50d78c
Austin-Didier Tran <tran.austin.didier@gmail.com>
2016-11-26 17:08:35 -0500

    Added gulp watch to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,14 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 
 gulp.task('default', () => {
-    console.log('hey');
+    gulp.src('public/app/js/*.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('server/dist/js'));
+});
+
+gulp.watch(['public/app/js/*.js'], (e) => {
     gulp.src('public/app/js/*.js')
         .pipe(babel({
             presets: ['es2015']
