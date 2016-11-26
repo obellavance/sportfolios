8ddcd141933838d10bc4594b066022ac90bfdab9
austindidiertran1 <tran.austin.didier@gmail.com>
2016-11-26 00:56:35 +0000

    Added gulp and changed command to npm start

--- public/dist/app.js
+++ public/dist/app.js
@@ -1,5 +1,7 @@
+"use strict";
+
 (function () {
-  const config = {
+  var config = {
     apiKey: "AIzaSyDD8I7t-oRml8ynG4FexBd_Tvu4WmqHXOw",
     authDomain: "sportfolios-3771b.firebaseapp.com",
     databaseURL: "https://sportfolios-3771b.firebaseio.com",
@@ -7,12 +9,10 @@
     messagingSenderId: "994977381143"
   };
   firebase.initializeApp(config);
-  
-  angular
-    .module('app', ['firebase'])
-    .controller('MyCtrl', function ($firebaseObject) {
-      const rootRef = firebase.database().ref().child('angular');
-      const ref = rootRef.child('object');
-      this.object = $firebaseObject(ref);
-    });
-}());
+
+  angular.module('app', ['firebase']).controller('MyCtrl', function ($firebaseObject) {
+    var rootRef = firebase.database().ref().child('angular');
+    var ref = rootRef.child('object');
+    this.object = $firebaseObject(ref);
+  });
+})();
