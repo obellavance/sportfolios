a7c40db200ae3b6e6b03524c024d9d7183907622
austindidiertran1 <tran.austin.didier@gmail.com>
2016-12-05 19:38:38 +0000

    Added picture

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ app.use(express.static(rootPath));
 
 // Map from the ES2015 file to the compiled ES5 file
 app.get('/app/js/:file', (req, res) => {
-  console.log(req.params.file);
   res.sendFile(path.join(__dirname + '/server/dist/js/' + req.params.file)); 
 });
 
